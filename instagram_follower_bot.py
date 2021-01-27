c6cecbf1163911774be7b2fb29715f79a0143010
priyamalytic <75631418+priyamalytic@users.noreply.github.com>
2021-01-27 19:18:12 +0530

    Update instagram_follower_bot.py

--- instagram_follower_bot.py
+++ instagram_follower_bot.py
@@ -44,14 +44,13 @@ class InstaFollower:
             time.sleep(2)
 
     def follow(self):
-        all_buttons = self.driver.find_elements_by_css_selector("li button")
+        all_buttons = self.driver.find_elements_by_xpath("//button[@class='sqdOP  L3NKy   y3zKF     ']")
         for button in all_buttons:
             try:
                 button.click()
                 time.sleep(1)
             except ElementClickInterceptedException:
-                cancel_button = self.driver.find_element_by_xpath(
-                    '/html/body/div[5]/div/div/div/div[3]/button[2]')
+                cancel_button = self.driver.driver.find_element_by_class_name("HoLwm")
                 cancel_button.click()
 
 bot = InstaFollower(chrome_driver_path)
